c37ed8cd599c387138313a2d99202d583fa76ba9
Ryan <ryanpaynt@uncc.edu>
2021-07-13 21:23:01 -0400

    committing to heroku

--- public/db.js
+++ public/db.js
@@ -26,3 +26,43 @@ const saveRecords = (record) => {
 
     objStore.add(record);
 }
+
+const checkDb = () => {
+    const trans = db.transaction(['newTrans'], 'readWrite');
+
+    const objStore = trans.objectStore('newTrans');
+    
+    const all = objStore.getAll();
+
+    all.onsuccess = () => {
+        if (all.result.length > 0){
+            fetch('api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(all.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message){
+                    throw new Error(serverResponse);
+                }
+
+                const trans = db.transaction(['newTrans'], 'readWrite');
+
+                const objStore = trans.objectStore('newTrans');
+
+                objStore.clear();
+
+                alert('All saved transactions have been submitted!');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        }
+    }
+}
+
+window.addEventListener('online', uploadTransaction);
