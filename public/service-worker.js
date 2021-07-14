db3d921a96d65f702ab9531028ffe147ea1cf969
Ryan <ryanpaynt@uncc.edu>
2021-07-13 22:39:47 -0400

    committing to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,6 @@ const FILES_TO_CACHE = [
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
   
-  // install
   self.addEventListener("install", function (evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
@@ -24,7 +23,6 @@ const FILES_TO_CACHE = [
     self.skipWaiting();
   });
   
-  // activate
   self.addEventListener("activate", function (evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
@@ -42,14 +40,12 @@ const FILES_TO_CACHE = [
     self.clients.claim();
   });
   
-  // fetch
   self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
             .then(response => {
-              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
@@ -57,7 +53,6 @@ const FILES_TO_CACHE = [
               return response;
             })
             .catch(err => {
-              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         }).catch(err => console.log(err))
@@ -72,7 +67,6 @@ const FILES_TO_CACHE = [
           if (response) {
             return response;
           } else if (evt.request.headers.get("accept").includes("text/html")) {
-            // return the cached home page for all requests for html pages
             return caches.match("/");
           }
         });
