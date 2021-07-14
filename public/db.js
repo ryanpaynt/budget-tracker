63293e3840a39910e5f00ee1c60033a55bbaa8b0
Ryan <ryanpaynt@uncc.edu>
2021-07-13 20:27:11 -0400

    three request functions made

--- public/db.js
+++ public/db.js
@@ -2,3 +2,21 @@ let db;
 
 const request = window.indexedDB.open('Budget-Tracker', 1);
 
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore('newTrans', {autoIncrement: true});
+};
+
+request.onsuccess = (event) => {
+    db = event.target.request;
+
+    if(navigator.onLine) {
+        uploadTransaction();
+    }
+};
+
+request.onerror = (event) => {
+    console.log(event.target.errorCode);
+};
+
+
