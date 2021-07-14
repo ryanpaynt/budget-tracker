05608a27dbffc6adcf7a56bb44ed201d8b3954c6
Ryan <ryanpaynt@uncc.edu>
2021-07-13 22:36:03 -0400

    committing to heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,6 @@ const FILES_TO_CACHE = [
     "/index.js", 
     "/favicon.ico", 
     "/styles.css", 
-    "/icons/icon-144x144.png",
     "/icons/icon-192x192.png", 
     "/icons/icon-512x512.png"
   ];
