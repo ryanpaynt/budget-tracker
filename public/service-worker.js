15c30a3eb6a75f56315d863b209c0db9a220b3b5
Ryan <ryanpaynt@uncc.edu>
2021-07-13 22:28:59 -0400

    fixing service.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,19 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "/icons/icon-192x192.png",
+    "/", 
+    "/index.html", 
+    "/index.js", 
+    "/favicon.ico", 
+    "/styles.css", 
+    "/icons/icon-144x144.png",
+    "/icons/icon-192x192.png", 
     "/icons/icon-512x512.png"
-];
-
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
-self.addEventListener("install", function (evt) {
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  // install
+  self.addEventListener("install", function (evt) {
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
         console.log("Your files were pre-cached successfully!");
@@ -19,7 +23,8 @@ self.addEventListener("install", function (evt) {
   
     self.skipWaiting();
   });
-
+  
+  // activate
   self.addEventListener("activate", function (evt) {
     evt.waitUntil(
       caches.keys().then(keyList => {
@@ -36,19 +41,23 @@ self.addEventListener("install", function (evt) {
   
     self.clients.claim();
   });
-
+  
+  // fetch
   self.addEventListener("fetch", function (evt) {
     if (evt.request.url.includes("/api/")) {
       evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
           return fetch(evt.request)
             .then(response => {
+              // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(evt.request.url, response.clone());
               }
+  
               return response;
             })
             .catch(err => {
+              // Network request failed, try to get it from the cache.
               return cache.match(evt.request);
             });
         }).catch(err => console.log(err))
@@ -56,15 +65,17 @@ self.addEventListener("install", function (evt) {
   
       return;
     }
+  
     evt.respondWith(
-        fetch(evt.request).catch(function () {
-          return caches.match(evt.request).then(function (response) {
-            if (response) {
-              return response;
-            } else if (evt.request.headers.get("accept").includes("text/html")) {
-              return caches.match("/");
-            }
-          });
-        })
-      );
-    });
+      fetch(evt.request).catch(function () {
+        return caches.match(evt.request).then(function (response) {
+          if (response) {
+            return response;
+          } else if (evt.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
+        });
+      })
+    );
+  });
